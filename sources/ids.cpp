941f48e8a4a39e70723bc5fe48601946db6bc404
Ahmedsaed <ahmedsaed2652003@gmail.com>
2023-05-21 20:11:09 +0300

    fix ids by starting the depth from 1

--- sources/ids.cpp
+++ sources/ids.cpp
@@ -1,7 +1,7 @@
 #include "algorithm.h"
 
 std::vector<std::string> iterativeDeepeningDFS(const std::string& source, const std::string& destination) {
-    int depthLimit = 0;
+    int depthLimit = 1;
     while (true) {
         std::vector<std::string> path = depthLimitedSearch(source, destination, depthLimit);
         if (!path.empty()) {
