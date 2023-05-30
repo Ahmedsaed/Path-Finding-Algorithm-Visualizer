f6a5d69d1412ec3b3b39d1ae3993774d32135a5f
Mazin Mohammad <mazinmohammad.w@gmail.com>
2023-05-31 01:05:50 +0300

    Added docstring for pair

--- include/pair.h
+++ include/pair.h
@@ -3,6 +3,24 @@
 
 #include "main.h"
 
+    /**
+     * @file pair.h
+     * @brief Implementation of a simple pair class template in C++.
+     *
+     * This file contains the header for a simple pair class template in C++,
+     * which is used to store two elements of potentially different types together.
+     *
+     * The implementation includes a class template, `pair`, which has two public
+     * data members, `first` and `second`, that store the two elements of the pair.
+     * The types of these elements are specified using the template parameters `t1`
+     * and `t2`, respectively.
+     *
+     * This class template is often used to simplify the handling of related data, such as
+     * coordinates, key-value pairs, or other pairs of values.
+     *
+     * @tparam t1 The type of the first element stored in the pair.
+     * @tparam t2 The type of the second element stored in the pair.
+     */
 template <typename t1, typename t2>
 class pair{
 public:
